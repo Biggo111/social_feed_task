e35fd605c77d96f46318013b2b9a82cdcf6115a2
Biggo111 <routh.biggo123@gmail.com>
2024-08-15 01:49:20 +0600

    network info

--- lib/core/network/network_info.dart
+++ lib/core/network/network_info.dart
@@ -5,7 +5,7 @@ abstract class NetworkInfo {
 class NetworkInfoImpl implements NetworkInfo {
   @override
   Future<bool> get isConnected async {
-    // Implementation for network connectivity check
-    return true; // Placeholder implementation
+    
+    return true;
   }
 }
